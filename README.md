73e4ee98133e2dc85206bb0c7b9e9dc4bbf37a10
Yuttasak Pannawat <yuttasakcom@gmail.com>
2019-06-05 00:30:24 +0700

    go-mongo-official-sample

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Go Mongo Official Sample
+
+## Start Docker-Compose
+
+```bash
+docker-compose up -d
+docker-compose exec mongo bash
+mongo -u admin -p secret --authenticationDatabase admin
+> use golang
+db.createUser({user: 'user', pwd: 'password',roles: [{ role: 'readWrite', db: 'golang' }]})
+```
+
+```bash
+go mod init github.com/yuttasakcom/go-mongo-official-sample
+```
--- main.go
+++ main.go
@@ -1,7 +1,59 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+type Users struct {
+	ID     primitive.ObjectID `json:"_id" bson:"_id"`
+	Name   string             `json:"name"`
+	Age    int                `json:"age"`
+	Status bool               `json:"status"`
+}
 
 func main() {
-	fmt.Println("Hello")
+	client, err := mongo.
+		NewClient(options.Client().
+			ApplyURI("mongodb://user:password@localhost:27017/golang"))
+
+	if err != nil {
+		log.Fatal("main: connot create new client. : ", err)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal("main: connot connect to database.")
+	}
+
+	collection := client.Database("golang").Collection("users")
+	ctx, cancelFindOne := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancelFindOne()
+	filter := bson.M{"name": "pi"}
+	singleResult := collection.FindOne(ctx, filter)
+
+	if err != nil {
+		log.Fatal("main : findOne : ", err)
+	}
+
+	result := Users{}
+	singleResult.Decode(&result)
+
+	resultByte, err := json.Marshal(result)
+	if err != nil {
+		log.Fatal("main : unabled to marshal json: ", result)
+	}
+
+	fmt.Println(string(resultByte))
 }
